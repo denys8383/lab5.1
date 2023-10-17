d4a42bff4ef5f137ef77d0c28aa422cd70f910ac
denys8383 <Denys.Barchuk.IK.2023@lpnu.ua>
2023-10-17 11:56:03 +0300

    commit 5.1

---
+++ Project5.1/Source.cpp
@@ -0,0 +1,19 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double k(const double x, const double y); // прототип
+int main()
+{
+	double p, q;
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	double c = (k(p + sqrt(q), q - sqrt(p)) * k(p + sqrt(q), q - sqrt(p))) - k(1, p + q);
+	cout << "c = " << c << endl;
+	return 0;
+}
+double k(const double x, const double y) // визначення
+{
+	return (x / (abs(x * x * x + y * y * y))) + (y / (abs(x + y)));
+}
+
